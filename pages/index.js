81cd182c295d33ecbd8632c02e775a6afb62de75
Talbekk <mt.campbell@outlook.com>
2022-10-02 01:05:56 +0100

    builds basic landing page

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,38 @@
+import Layout from '../components/layout';
 import Head from 'next/head';
-import Layout, { siteTitle } from '../components/layout';
-import utilStyles from '../styles/utils.module.css';
+import Image from 'next/image';
+import { Box, Button, Card, TextField, Typography } from '@mui/material';
+import ArrowRightAltIcon from '@mui/icons-material/ArrowRightAlt';
+import InstagramIcon from '@mui/icons-material/Instagram';
 
 export default function Home() {
   return (
-    <Layout home>
+    <Layout>
       <Head>
-        <title>{siteTitle}</title>
+        <title>Stranger's Point Gin</title>
       </Head>
-      <section className={utilStyles.headingMd}>
-        <p>Coming soon...</p>
-        <p>Sign up here.</p>
-        <p>Instagram</p>
-      </section>
+      <Box sx={{ minWidth: 275 }}>
+        <Card variant="outlined" className='main-container'>
+          <Image
+            priority
+            src="/images/profile.png"
+            height={175}
+            width={325}
+            alt="Strangers Point Gin Logo"
+          />
+        <Typography variant="body2" align="center" gutterBottom={true} marginTop={5}>Coming to you very soon!</Typography>
+          <Typography variant="body2" align="center" gutterBottom={true}>Sign up below to keep up to date with our progress.</Typography>
+          <div className="sign-up-row">
+            <TextField id="filled-basic" className="sign-up-form" variant="outlined" size="small"/>
+            <Button variant="contained" className="sign-up-btn">Sign Up</Button>
+          </div>
+          <div className="section-instagram">
+            <Typography variant="body2" aligh="center">Follow us</Typography>
+            <ArrowRightAltIcon/>
+            <InstagramIcon/>
+          </div>
+        </Card>
+      </Box>
     </Layout>
   );
 }
